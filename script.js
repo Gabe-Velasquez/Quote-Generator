7b2ebf0fc08b782d572793b556caedd17c1a7961
Gabe <gabriel.velasquez0116@gmail.com>
2023-06-12 14:10:25 -0600

    added loader

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true; 
+}
+
+// Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 //Show new quote
 function newQuote(){
+    loading();
+    // Random used to fetch from pool of quotes
     const quote = apiQuotes[Math.floor(Math.random() *apiQuotes.length)];
-
     //Checking if author is present
     if (!quote.author){
         authorText.textContent = "Unknown";
@@ -23,11 +36,14 @@ function newQuote(){
     }else{
         quoteText.classList.remove('long-quote');
     }
+    // Set quote and hides loader
     quoteText.textContent = quote.text
+    complete();
 }
 
 // Gather quotes from API
 async function getQuotes() {
+    loading();
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
     try{
         const resposne = await fetch (apiURL);
